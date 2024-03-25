ebd1d8e496021d823df0e8b8b6153f578c01b00a
zorth <nicolaszorth@outlook.com>
2024-03-25 16:23:15 +0800

    修改了一些规则

--- src/main/java/org/zorth/Service/impl/BookServiceImpl.java
+++ src/main/java/org/zorth/Service/impl/BookServiceImpl.java
@@ -3,17 +3,23 @@ package org.zorth.Service.impl;
 import org.springframework.stereotype.Service;
 import org.zorth.Service.BookService;
 import org.zorth.mapper.BookMapper;
+import org.zorth.mapper.ReadingProgressMapper;
 import org.zorth.pojo.Book;
+import org.zorth.pojo.ReadingProgress;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class BookServiceImpl implements BookService {
 
     private final BookMapper bookMapper;
+    private final ReadingProgressMapper readingProgressMapper;
 
-    public BookServiceImpl(BookMapper bookMapper) {
+    public BookServiceImpl(BookMapper bookMapper, ReadingProgressMapper readingProgressMapper) {
         this.bookMapper = bookMapper;
+        this.readingProgressMapper = readingProgressMapper;
     }
 
     @Override
@@ -35,6 +41,19 @@ public class BookServiceImpl implements BookService {
     @Override
     public Book updateBook(Book book) {
         bookMapper.updateBook(book);
+        ReadingProgress progress = new ReadingProgress();
+        progress.setBookId(book.getBookId());
+        LocalDateTime endTime = LocalDateTime.now(); // 获取当前时间
+        Timestamp timestamp = Timestamp.valueOf(endTime); // 转换为 Timestamp 类型
+        progress.setEndTime(timestamp);
+
+        // 根据 EndTime 计算 StartTime
+        LocalDateTime startTime = endTime.minusMinutes(book.getTotalReadingDurationMinutes());
+        Timestamp startTimestamp = Timestamp.valueOf(startTime);
+        progress.setStartTime(startTimestamp);
+
+        progress.setDurationMinutes(book.getTotalReadingDurationMinutes());
+        readingProgressMapper.insertProgress(progress);
         return book;
     }
 
